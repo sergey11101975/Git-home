41ba8dd1f296bb04f4dd1a5fbb7a2a96acfd9074
Sergey11101975 <bogoslovskiisv@yandex.ru>
2023-05-23 11:25:59 +0300

    урок

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+double numberA = new Random().Next(1, 10); // 1,2,3...
+Console.Write(numberA);
+double numberB = new Random().Next(1, 5);
+Console.WriteLine(numberB);
+double result = numberA + numberB;
+Console.WriteLine(result);
